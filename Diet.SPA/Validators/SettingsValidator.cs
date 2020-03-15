d9d7ec2fa4e79194d7d0b910fd0078107b507743
Ihor Romanchuk <ihorr.romanchuk@gmail.com>
2020-03-15 15:38:13 +0200

    Fixed settings validation.

--- Diet.SPA/Validators/SettingsValidator.cs
+++ Diet.SPA/Validators/SettingsValidator.cs
@@ -8,20 +8,13 @@ namespace Diet.SPA.Validators
         public SettingsValidator()
         {
             RuleFor(x => x.Value).NotNull().NotEmpty().WithMessage("This field can't be empty");
-            RuleFor(x => x.Value).Must(settingsValue => ValidateSettings(settingsValue)).WithMessage("Calories per day must be a number and less than 30000");
+            RuleFor(x => x.Value).Must(settingsValue => ValidateSettings(settingsValue)).WithMessage("Calories per day must be a positive number and less than 30000");
         }
 
         protected bool ValidateSettings(string settingValue)
         {
             bool isNumeric = int.TryParse(settingValue, out int limit);
-            if (isNumeric)
-            {
-                return limit > 30000 ? false : true;
-            }
-            else 
-            {
-                return false;
-            }
+            return isNumeric && limit > 0 && limit < 30000;
         }
     }
 }
